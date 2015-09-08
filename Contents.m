52b28339efb97ec2b659d412a2151dffa15e8d2b
Marko Budisic <mbudisic@gmail.com>
2015-09-08 16:35:45 -0500

    Added Contents.m

---
+++ Contents.m
@@ -0,0 +1,6 @@
+Demo of a workflow for Software workshop in the Math department.
+
+%   generate  - Generate data for Software Workshop.
+%   plotall   - Plot all trajectories.
+%   onescript - Example of the prototyped run.
+%   run       - Visualize a data run.
